a04a3986afc0a2946127be32f05444186b2889ed
Tom Jose <tomjose92@gmail.com>
2024-03-21 07:27:06 +0000

    Updated Dashboard to use Segment queries for stability for now

--- pages/Dashboard/jsobjects/Actions/Actions.js
+++
@@ -1,8 +0,0 @@
-export default {
-	onPageLoad: () => {
-		getSalesforceOpportunities.run().then(() => {
-			getAllUsage.run();
-			getSalesforceZendeskCases.run();
-		})
-	}
-}
--- pages/Dashboard/jsobjects/SalesforceUtils/SalesforceUtils.js
+++ pages/Dashboard/jsobjects/SalesforceUtils/SalesforceUtils.js
@@ -1,7 +1,26 @@
 export default {
 	getAccounts: () => {
-		let annualOpps = getSalesforceOpportunities.data.records;
+		let annualOpps = getSegmentSFOpportunities.data.map(item => {
+			let account = getSegmentSFAccounts.data.find(account => account.id === item.account_id);
+			return ({
+				"Amount": item.amount,
+				"Account": {
+					"Id": item.account_id,
+					"Name": account.name,
+					"Stripe_ID__c": account.stripe_id_c,
+				},
+				"Owner": {
+					"Name": getSegmentSFUsers.data.find(user => user.id === item.owner_id).first_name,
+				},
+				"Cases": getSegmentSFCases.data.filter(cases => cases.account_id === item.account_id).length,
+				"Usage": getAllUsage.data.find(item => item.stripe_customer_id === account.stripe_id_c)?.total_sessions || 0,
+				"Contract_Start_Date__c": item.contract_start_date_c,
+				"Contract_End_Date__c": item.contract_end_date_c,
+				"ARR__c": item.arr_c
+			})
+		});
 		let uniqueAccounts = _.uniqBy(annualOpps, "Account.Name");
 		return uniqueAccounts;
-	}
+	},
+	getAllAccountIds: () => _.uniqBy(getSegmentSFOpportunities.data, "account_id").map(item => item.account_id)
 }
