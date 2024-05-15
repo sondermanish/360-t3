f3e53a2d2183c2b0e0bc3b75b602b46ba5c67597
Tom Jose <tomjose92@gmail.com>
2024-05-15 11:25:23 +0000

    Fixed all issues with respect to the latest salesforce migration and re synchronization of Salesforce on Segment warehouse. Updated the packages to use is_deleted = false in queries

--- pages/Sales/jsobjects/Constants/Constants.js
+++ pages/Sales/jsobjects/Constants/Constants.js
@@ -4,6 +4,6 @@ export default {
 		"next_step", "type", "lead_source", "close_date", "probability", "arr_c",
 		"mrr_c", "loss_reason_c", "loss_reason_detail_c", "closed_won_details_c", "use_case_c", "created_date", "sales_engineer_c"],
 	AccountQueryFields: ["id", "name", "description", "website", "number_of_employees", "industry", "billing_country", "stripe_id_c", "customer_segmentation_c"],
-	UserQueryFields: ["id", "username", "title", "first_name", "last_name", "email",  "time_zone_sid_key"],
+	UserQueryFields: ["id", "username", "first_name", "last_name", "email",  "time_zone_sid_key"],
 	ContactQueryFields: ["account_id", "id", "name", "title", "email"]
 }
